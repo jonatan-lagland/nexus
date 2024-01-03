15324a6b09acc8f2d5eba307d705cc9d96874a37
jonatan-lagland <jonatan.lagland@gmail.com>
2024-01-03 20:11:25 +0200

    Fix an issue with imports in error.js and Runes.jsx

--- app/error.js
+++ app/error.js
@@ -1,5 +1,6 @@
 'use client'
 import { error500 } from "@utils/errorResponses";
+import ErrorPage from "@components/ErrorPage";
 
 export default function Error({ error, reset, }) {
     return (
--- components/Profile/Section/Runes.jsx
+++ components/Profile/Section/Runes.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image';
+import Tooltip from './Tooltip';
 
 function Runes({ runes, title }) {
 
