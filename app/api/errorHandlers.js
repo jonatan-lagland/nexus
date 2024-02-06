2ebafda7536f35630d76cd34d40313dd0d844a4f
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-06 03:17:19 +0200

    Move rate limit handler to a seperate function

--- app/api/errorHandlers.js
+++ app/api/errorHandlers.js
@@ -3,46 +3,47 @@
 export async function serverErrorHandler(response) {
     switch (response.status) {
         case 408:
-            throwTimeOutError();
+            await throwTimeOutError();
             break;
         case 429:
-            await rateLimitHandler();
+            await throwRateLimitError();
             break;
         default:
-            throwServerError()
+            await throwServerError()
             break;
     }
 
-    function throwServerError() {
+    async function throwServerError() {
         const error = new Error('An error has occurred. Please try again later.');
         error.status = response.status;
         error.reason = "Server Error"
         throw error;
     }
 
-    function throwTimeOutError() {
+    async function throwTimeOutError() {
         const error = new Error('We were unable to resolve a response from Riot Games API. Please try again later.');
         error.status = 408;
         error.reason = "Request Timeout"
         throw error;
     }
 
-    /* In the event of a 429 Rate Limit error from Riot API, create a Promise that resolves after the time in Retry-After header is over */
-
-    async function rateLimitHandler() {
-        console.log("Rate limit handler initiating...")
+    async function throwRateLimitError() {
         const retryAfter = response.headers.get('Retry-After');
-        const delayInMilliseconds = parseInt(retryAfter) * 1000;
-        const delayPromise = new Promise(resolve => {
-            setTimeout(() => {
-                resolve(delayInMilliseconds);
-            }, delayInMilliseconds);
-        });
-
-        await delayPromise;
         const error = new Error('Rate limit was reached.');
         error.status = 429;
         error.reason = "Rate Limit"
+        error.retryAfter = retryAfter;
         throw error;
     }
+}
+
+/* In the event of a 429 Rate Limit error from Riot API, create a Promise that resolves after the time in Retry-After header is over */
+export async function rateLimitHandler(retryAfter) {
+    const delayInMilliseconds = parseInt(retryAfter) * 1000;
+    const delayPromise = new Promise(resolve => {
+        setTimeout(() => {
+            resolve(delayInMilliseconds);
+        }, delayInMilliseconds);
+    });
+    await delayPromise;
 }
