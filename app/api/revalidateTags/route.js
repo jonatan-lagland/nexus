24a6919f0cfd1e3712d206b81ad6507e4b821edf
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-06 03:06:59 +0200

    Implement an API route for revalidating static data

---
+++ app/api/revalidateTags/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import { revalidateTag } from 'next/cache';
+
+export function GET() {
+    const tagsToRevalidate = ['items', 'championList', 'latest_version', 'runes'];
+
+    try {
+        for (const tag of tagsToRevalidate) {
+            revalidateTag(tag);
+        }
+        return NextResponse.json({ success: 'Tags revalidated successfully' }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
+}
