471757f9e8a800063f93caa5b6f1920c362c4dd8
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-06 03:12:33 +0200

    Refractor fetchDataHandler and unit tests for the handler

--- app/api/fetchDataHandler.js
+++ app/api/fetchDataHandler.js
@@ -1,55 +1,18 @@
 'use server'
 import { serverErrorHandler } from "./errorHandlers";
 
-export default async function fetchDataHandler(url, includeApiKey = false, timeoutDuration = 0) {
-    // By default, data fetching has no set timeout. TimeoutDuration above 0 indicates a time limit should be set.
+export default async function fetchDataHandler(url, tag, includeApiKey = false) {
     // By default, no API key is included in header. API key isn't needed when fetching data from DDragon API.
-    // A timeout can be manually set in each API route, often when fetching crucial data like match history data.
-
-    if (timeoutDuration > 0) {
-        const response = await fetchWithTimeout();
-        return response;
-    } else {
-        const response = await standardFetch();
-        return response;
+    const headers = {};
+    if (includeApiKey) {
+        headers[process.env.API_KEY] = process.env.API_KEY_VALUE;
     }
-
-    async function standardFetch() {
-        const headers = {};
-        if (includeApiKey) {
-            headers[process.env.API_KEY] = process.env.API_KEY_VALUE;
-        }
-        const response = await fetch(url, { headers });
-        console.log(response.status)
-
-        if (!response.ok) {
-            await serverErrorHandler(response);
-        }
-        return response.json();
+    const response = await fetch(url, {
+        headers,
+        next: { tags: [tag] }
+    });
+    if (!response.ok) {
+        await serverErrorHandler(response);
     }
-
-    async function fetchWithTimeout() {
-        const headers = {};
-        if (includeApiKey) {
-            headers[process.env.API_KEY] = process.env.API_KEY_VALUE;
-        }
-        const timeout = new Promise((_, reject) => {
-            setTimeout(() => {
-                const error = new Error('We were unable to resolve a response from Riot Games API. Please try again later.');
-                error.status = 408;
-                error.reason = "Request Timeout"
-                reject(error)
-            }, timeoutDuration);
-        });
-
-        const response = await Promise.race([
-            fetch(url, { headers }),
-            timeout
-        ]);
-
-        if (!response.ok) {
-            await serverErrorHandler(response);
-        }
-        return response.json();
-    }
+    return response.json();
 }
--- jest/utils/fetchDataHandler.test.js
+++ jest/utils/fetchDataHandler.test.js
@@ -33,26 +33,3 @@ it('should handle network failure when attempting to fetch data and return a 500
         expect(error.reason).toEqual('Server Error');
     }
 });
-
-it('should handle network failure when fetching mock data reaches set time limit', async () => {
-    // Mock fetch to delay indefinitely
-    jest.spyOn(global, 'fetch').mockImplementation(() =>
-        new Promise(() => {
-            // Do not resolve or reject to simulate a delayed response
-        })
-    );
-    try {
-        // Data handler with a timeout set to 2000 ms
-        const INCLUDE_API_KEY = false;
-        await fetchDataHandler('https://example.com', INCLUDE_API_KEY, 2000);
-        // Fail test if above line does not throw
-        expect(true).toBe(false);
-    } catch (error) {
-        expect(error.message).toEqual('We were unable to resolve a response from Riot Games API. Please try again later.');
-        expect(error.status).toEqual(408);
-        expect(error.reason).toEqual("Request Timeout");
-    }
-
-    // Restore fetch to its original implementation
-    global.fetch.mockRestore();
-});
