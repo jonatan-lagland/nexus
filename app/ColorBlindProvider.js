3e82f0a4a7b863ab3789eaaa1ef04e9e1e78c3db
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-17 01:53:26 +0200

    Refractor ColorBlindProvider to save selected option in local storage

--- app/ColorBlindProvider.js
+++ app/ColorBlindProvider.js
@@ -1,13 +1,28 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ColorblindContext } from "@utils/context/colorBlindContext";
 
 function ColorBlindProvider({ children }) {
-    const [isColorblindMode, setIsColorblindMode] = useState(false);
+    // Initialize state with value from local storage
+    const [isColorblindMode, setIsColorblindMode] = useState(() => {
+        // Check for window and localStorage availability
+        if (typeof window !== 'undefined') {
+            const localData = localStorage.getItem('colorblindMode');
+            return localData ? JSON.parse(localData) : false;
+        }
+        return false;
+    });
 
     const toggleColorblindMode = () => {
         setIsColorblindMode(!isColorblindMode);
     };
+
+    // Update local storage when state changes
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('colorblindMode', JSON.stringify(isColorblindMode));
+        }
+    }, [isColorblindMode]);
 
     return (
         <ColorblindContext.Provider value={{ isColorblindMode, toggleColorblindMode }}>
