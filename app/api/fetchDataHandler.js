b595a9d0edf76c207106a3ed0056fa90e464b39a
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-17 01:46:20 +0200

    Refractor fetchDataHandler to be able to retry fetch in the event of a rate limit

--- app/api/fetchDataHandler.js
+++ app/api/fetchDataHandler.js
@@ -1,18 +1,30 @@
 'use server'
 import { serverErrorHandler } from "./errorHandlers";
+import { rateLimitHandler } from "./errorHandlers";
 
-export default async function fetchDataHandler(url, tag, includeApiKey = false) {
+export default async function fetchDataHandler(url, tag, includeApiKey = false, cacheDuration = 'false') {
     // By default, no API key is included in header. API key isn't needed when fetching data from DDragon API.
     const headers = {};
     if (includeApiKey) {
         headers[process.env.API_KEY] = process.env.API_KEY_VALUE;
     }
-    const response = await fetch(url, {
-        headers,
-        next: { tags: [tag] }
-    });
-    if (!response.ok) {
-        await serverErrorHandler(response);
+
+    // In case of a rate limit error, attempt fetch for a total of 3 times after delay
+    const maxRetries = 3;
+
+    for (let attempt = 0; attempt < maxRetries; attempt++) {
+        const response = await fetch(url, {
+            headers,
+            next: { tags: [tag], revalidate: cacheDuration }
+        });
+        if (response.status === 429 && attempt < maxRetries - 1) {
+            const retryAfter = response.headers.get('Retry-After');
+            await rateLimitHandler(retryAfter)
+            continue; // Jump back to start of the loop and retry the fetch
+        }
+        if (!response.ok) {
+            await serverErrorHandler(response);
+        }
+        return response.json();
     }
-    return response.json();
 }
