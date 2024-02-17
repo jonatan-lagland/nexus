63248d8d01594a4d622b75f74f3b45560d042bfe
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-17 02:09:13 +0200

    Refractor profile sidebar layout

--- components/Profile/Sidebar/Sidebar.jsx
+++ components/Profile/Sidebar/Sidebar.jsx
@@ -3,28 +3,36 @@ import React from 'react'
 import Image from 'next/image'
 
 function Sidebar({ rankedDetails }) {
-
-    const playerTier = rankedDetails[0] && rankedDetails[0].tier ? rankedDetails[0].tier : "unranked";
+    const playerTier = rankedDetails[0] && rankedDetails[0].tier ? rankedDetails[0].tier : "Unranked";
     const playerRank = rankedDetails[0] && rankedDetails[0].rank ? rankedDetails[0].rank : null;
-    const leaguePoints = rankedDetails[0] && rankedDetails[0].leaguePoints ? rankedDetails[0].leaguePoints + " lp" : null;
+    const leaguePoints = rankedDetails[0] && rankedDetails[0].leaguePoints ? rankedDetails[0].leaguePoints + " lp" : rankedDetails[0] && rankedDetails[0].tier ? "0 lp" : null;
+    const wins = rankedDetails[0] && rankedDetails[0].wins ? rankedDetails[0].wins : 0;
+    const losses = rankedDetails[0] && rankedDetails[0].losses ? rankedDetails[0].losses : 0;
 
     return (
-        <section className="profile-grid-section bg-slate-500">
-            <div className='flex flex-col justify-center items-center'>
-                <div>
-                    <Image
-                        src={`https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/images/${playerTier.toLowerCase()}.png`}
-                        alt={`${playerTier} Emblem`}
-                        height={120}
-                        width={120}
-                        priority
-                        className='select-none'
-                    >
-                    </Image>
-                </div>
-                <div className='text-center text-blue-200 drop-shadow-md'>
-                    <h1 className='text-2xl font-bebas'>{playerTier} {playerRank}</h1>
-                    <h2 className='text-lg font-bebas'>{leaguePoints}</h2>
+        <section className="">
+            <div className='flex flex-col bg-deep-purple border border-gray-600 rounded-lg py-8 px-16'>
+                <div className='flex flex-row justify-between items-center'>
+                    <div style={{ textShadow: "1px 1px 1px black" }} className='flex flex-col drop-shadow-md '>
+                        <span className='text-2xl lg:text-3xl font-bold text-white'>{playerTier} {playerRank}</span>
+                        <span className='text-lg lg:text-xl text-gray-400'>{leaguePoints}</span>
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <Image
+                            src={`https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/images/${playerTier.toLowerCase()}.png`}
+                            alt={`${playerTier} Emblem`}
+                            height={128}
+                            width={128}
+                            quality={100}
+                            priority
+                            className='select-none'
+                        >
+                        </Image>
+                        <div style={{ textShadow: "1px 1px 1px black" }} className='flex flex-row gap-3 text-gray-400'>
+                            <span>Wins <span className=' font-semibold text-gray-300'>{wins}</span></span>
+                            <span>Losses <span className=' font-semibold text-gray-300'>{losses}</span></span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
