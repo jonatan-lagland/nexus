bc1dd9657fc2ec5f59a60f9619d6bccbaacff27d
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-17 02:10:50 +0200

    Include cdn.communitydragon.org in next.config remotePatterns list

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
@@ -20,6 +24,12 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
+                hostname: 'cdn.communitydragon.org',
+                port: '',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
                 port: '',
                 pathname: '**'
@@ -35,4 +45,4 @@ const nextConfig = {
     }
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
