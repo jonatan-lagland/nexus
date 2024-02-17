3e99beb63525c4c87d28bd2d44c33fdf6d9523ca
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-17 02:06:57 +0200

    Implement fetch objects in profile component for summoner spells, queue types and live game

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -1,58 +1,61 @@
 'use server'
-import React from 'react'
 import Header from '@components/Profile/Header/Header'
-import Section from '@components/Profile/Section/Section'
+import Section from './Section/Section'
 import Sidebar from '@components/Profile/Sidebar/Sidebar'
 import { StaticDataProvider } from '@app/StaticDataProvider'
 import { getUserInfo, getUserPUUID, getRankedInfo } from '@app/api/userProps'
 import { getChampionListProps } from '@app/api/championProps'
 import { getItemProps } from '@app/api/itemProps'
-import { getRuneProps } from '@app/api/runeProps'
-import { getLatestVersion } from '@app/api/latestVersion'
-import { QueryProvider } from '@app/api/QueryClientProvider'
+import { getRuneProps, getSummonerSpellProps } from '@app/api/spellProps'
+import { getLatestVersion, getQueueTypes } from '@app/api/gameData'
+import { QueryProvider } from '@app/QueryClientProvider'
 import { getMatchHistory } from '@app/api/userProps'
 import { MatchHistoryProvider } from '@app/MatchHistoryProvider'
+import { LiveGameProvider } from '@app/LiveGameProvider'
 
 async function Profile({ params, region, server }) {
-
     const gameVersion = await getLatestVersion();
-
-    const [championList, itemList, runeList] = await Promise.all([
+    const queueTypes = await getQueueTypes();
+    const [championList, itemList, runeList, summonerSpellList] = await Promise.all([
         getChampionListProps(gameVersion),
         getItemProps(gameVersion),
-        getRuneProps(gameVersion)
+        getRuneProps(gameVersion),
+        getSummonerSpellProps()
     ])
 
-    const userData = await getUserPUUID(params, region);
-    const userDetailsData = await getUserInfo(userData.puuid, server);
-    const rankedDetailsData = await getRankedInfo(userDetailsData.id, server);
-    const matchHistoryData = await getMatchHistory(userData.puuid, region);
-    const [user, userDetails, rankedDetails, matchHistory] = await Promise.all([userData, userDetailsData, rankedDetailsData, matchHistoryData])
+    const user = await getUserPUUID(params, region);
+    const userDetails = await getUserInfo(user.puuid, server);
+    const rankedDetails = await getRankedInfo(userDetails.id, server);
+    const matchHistory = await getMatchHistory(user.puuid, region);
 
     const data = {
         championList,
         itemList,
         runeList,
+        summonerSpellList,
+        queueTypes,
         gameVersion,
     };
 
     return (
         <QueryProvider>
             <StaticDataProvider data={data}>
-                <MatchHistoryProvider matchHistory={matchHistory}> {/* Set initial data of Match History */}
-                    <section className='profile'>
-                        <section className='grid'>
-                            <article>
-                                <Header user={user} userDetails={userDetails} rankedDetails={rankedDetails} region={region} />
-                            </article>
-                            <article>
-                                <Section user={user} region={region} />
-                            </article>
-                            <article className="bg-deep-purple">
-                                <Sidebar rankedDetails={rankedDetails} />
-                            </article>
+                <MatchHistoryProvider matchHistory={matchHistory} rankedDetails={rankedDetails}> {/* Set initial data of Match History */}
+                    <LiveGameProvider>
+                        <section className='profile'>
+                            <section className='grid'>
+                                <article>
+                                    <Header rankedDetails={rankedDetails} user={user} region={region} server={server} userDetails={userDetails} />
+                                </article>
+                                <article>
+                                    <Section user={user} region={region} />
+                                </article>
+                                <article>
+                                    <Sidebar rankedDetails={rankedDetails} />
+                                </article>
+                            </section>
                         </section>
-                    </section>
+                    </LiveGameProvider>
                 </MatchHistoryProvider>
             </StaticDataProvider>
         </QueryProvider>
