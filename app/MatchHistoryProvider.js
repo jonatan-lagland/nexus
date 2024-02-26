3c9a561b79b2ff6aef2b1720c990b368acbff1e1
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-26 19:07:16 +0200

    Refractor Query Client for match history

--- app/MatchHistoryProvider.js
+++ app/MatchHistoryProvider.js
@@ -4,21 +4,21 @@ import React from 'react';
 import { useState } from 'react';
 import { MatchHistoryContext } from '@utils/context/matchHistoryContext';
 import { getMatchHistory, getRankedInfo } from './api/userProps';
+import { useQueryClient } from '@tanstack/react-query';
 
-export const MatchHistoryProvider = ({ children, matchHistory, rankedDetails }) => {
+export const MatchHistoryProvider = ({ children, matchHistory }) => {
     const [matchHistoryData, setMatchHistoryData] = useState(matchHistory);
-    const [rankedDetailsData, setrankedDetailsData] = useState(rankedDetails);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    const queryClient = useQueryClient();
 
-    const refreshMatchHistoryData = (puuid, region, server, leagueId) => {
+    const refreshMatchHistoryData = (puuid, region, server, summonerId) => {
         const fetchData = async () => {
             setIsLoading(true)
             try {
                 const refreshCache = true;
-                if (leagueId) {
-                    const res_ranked = await getRankedInfo(leagueId, server, refreshCache);
-                    setrankedDetailsData(res_ranked)
+                if (summonerId) {
+                    await getRankedInfo(summonerId, server, refreshCache);
                 }
                 const res_match = await getMatchHistory(puuid, region, refreshCache);
                 setMatchHistoryData(res_match);
@@ -31,8 +31,15 @@ export const MatchHistoryProvider = ({ children, matchHistory, rankedDetails }) 
         fetchData();
     };
 
+    const invalidateQuery = () => {
+        queryClient.invalidateQueries('query', {
+            refetchActive: true,
+            refetchInactive: false,
+        });
+    }
+
     return (
-        <MatchHistoryContext.Provider value={{ refreshMatchHistoryData, matchHistoryData, isLoading, error, rankedDetailsData }}>
+        <MatchHistoryContext.Provider value={{ refreshMatchHistoryData, matchHistoryData, isLoading, error, invalidateQuery }}>
             {children}
         </MatchHistoryContext.Provider>
     );
--- app/QueryClientProvider.js
+++ app/QueryClientProvider.js
@@ -3,9 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
 export const QueryProvider = ({ children }) => {
-    const [queryClient] = React.useState(() => new QueryClient({
-        defaultOptions: { queries: { staleTime: 172800000 } }, // 48 hours
-    }))
+    const queryClient = new QueryClient()
 
     return (
         <QueryClientProvider client={queryClient}>
