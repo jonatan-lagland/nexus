c75ccf0afde3287800aab7f3c74752b16db4b9b9
jonatan-lagland <jonatan.lagland@gmail.com>
2024-02-28 19:48:49 +0200

    Fix an issue with QueryClient data becoming instantly stale

--- app/QueryClientProvider.js
+++ app/QueryClientProvider.js
@@ -3,7 +3,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
 export const QueryProvider = ({ children }) => {
-    const queryClient = new QueryClient()
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: Infinity,
+            },
+        },
+    })
 
     return (
         <QueryClientProvider client={queryClient}>
