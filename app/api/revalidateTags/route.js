c11c7703164699b1de1087dcf6d7b832d79de968
jonatan-lagland <jonatan.lagland@gmail.com>
2024-07-05 11:21:42 +0300

    Introduce cron jobs for static data revalidation once a day at 5 AM UTC

--- app/api/revalidateTags/route.js
+++ app/api/revalidateTags/route.js
@@ -1,15 +1,21 @@
-import { NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 
-export function GET() {
+export async function GET(req) {
     const tagsToRevalidate = ['items', 'championList', 'latest_version', 'runes', 'queue_types'];
+    const authHeader = req.headers.get('authorization');
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    }
 
     try {
         for (const tag of tagsToRevalidate) {
             revalidateTag(tag);
         }
-        return NextResponse.json({ success: 'Tags revalidated successfully' }, { status: 200 })
+        return Response.json({ success: 'Tags revalidated successfully' }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error: error }, { status: 500 })
+        return Response.json({ error: error }, { status: 500 });
     }
 }
